eba74bdbda1d61c942e25018b9aba7698a511f8c
juwonkim0225-ship-it <juwonkim0225@gmail.com>
2026-05-31 02:50:25 +0900

    Fix iPhone PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "workout-app-v1";
+const CACHE_NAME = "workout-app-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./style.css",
     "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event=>{
@@ -13,6 +15,24 @@ self.addEventListener("install", event=>{
         caches.open(CACHE_NAME)
         .then(cache=>cache.addAll(FILES_TO_CACHE))
     );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event=>{
+    event.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(
+                keys.map(key=>{
+                    if(key !== CACHE_NAME){
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event=>{
